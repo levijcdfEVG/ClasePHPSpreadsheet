927581f5b66e67f0c40d4416159b760b503240dc
Levi Josué Candeias de Figueiredo <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-18 07:52:54 +0100

    Se han arreglado las rutas

---
+++ paginaBase/crearExcel.php
@@ -0,0 +1,22 @@
+<?php
+
+//Fuente: https://phpspreadsheet.readthedocs.io/en/latest/
+
+
+require '/vendor/autoload.php';
+
+use \PhpOffice\PhpSpreadsheet\Spreadsheet;
+use \PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+//Creamos el objeto de hoja de calculo
+$hojaDeCalculo = new Spreadsheet();
+$activeWorksheet = $hojaDeCalculo->getActiveSheet();
+for ($i=1; $i < 10; $i++) { 
+    $activeWorksheet->setCellValue('A'.$i, '¡Hola mundo!');
+}
+
+
+//Crear el objeto que va a escribir la hoja de calculo
+$writer = new Xlsx($spreadsheet);
+$writer->save('holaMundo.xlsx');
